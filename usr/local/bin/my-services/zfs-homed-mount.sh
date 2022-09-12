205f192f128ba7a2f9b79e640bbc7235965f5877
jkool702 <107448833+jkool702@users.noreply.github.com>
2022-09-12 10:57:45 -0400

    tmpfile job control and sxtra sanity checks
    Initial commit that adds tmpfile-based job control - only one instance of zfs_homed_mount per user will run at a time...additional calls will get queued up. Also added extra sanity checks, and including checks so that if additional zfshomed_mount calls are queued up and arent need they will see that everything was already mounted correctly and exit early.

--- usr/local/bin/my-services/zfs-homed-mount.sh
+++ usr/local/bin/my-services/zfs-homed-mount.sh
@@ -5,71 +5,128 @@ zfs_homed_mount() {
 	local username
 	local tries
 	local zfsHomedRoot
-
+	local tmpdir
+	
 	# echo "running zfs_homed_mount" >&2
 
+	# if no zfs dataset named HOMED exists then exit
 	zfsHomedRoot="$(zfs list -o name -H | grep -i -E 'HOMED$')"
 	[[ -z ${zfsHomedRoot} ]] && return 0
 
 	username="${1}"
-	tries=50
+	tries=0
 
-
+	# wait for systemd-homed to finish activating the users home
+	# if given user name wait up to 100 sec. If not given user name, wait up to 10 sec
 	if [[ -z "${username}" ]]; then
-		while (( ${tries} > 0 )); do
-                	username="$(find /home/*/.zfs/key.zfs -type f 2>/dev/null)" && break
-			((tries--))
-			sleep 0.2s
+	
+		# If username not given, monitor /home for the appearance of a keyfile. This should only be present after systemd-homed has finished activating but before the ZFS home dir is mounted. It appearing probably means that is the user whose home area was just activated.
+		while (( ${tries} < 50 )); do
+			username="$(find /home/*/.zfs/key.zfs -type f 2>/dev/null)" && break
 			
+			((tries++))
+			sleep 0.2s
 		done
-	        username="$(echo "${username}" | awk -F '/' '{print $3}')"
+		username="$(echo "${username}" | awk -F '/' '{print $3}')"
+		
+		# if ZFS homedir is already mounted and the identity file already bind-mounted then this is a unneeded duplicate call, so return
+		{ [[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] && cat /proc/mounts | greq -q -F "/home/${username}/.identity"; } && return 0
+		
 	else
-		[[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] && zfs umount "${zfsHomedRoot}/${username}/data"
-		while (( ${tries} > 0 )); do
-			[[ "$(homectl inspect "${username}" | grep 'State' | sed -E s/'.*State\: '//)" == 'active' ]] && break
-			# [[ -f "$(zfs get keylocation "${zfsHomedRoot}/${username}/data" -H -o value | sed -E s/'^file\:\/\/'//)" ]] && break
+		# if ZFS homedir is already mounted and the identity file already bind-mounted then this is a unneeded duplicate call, so return
+		{ [[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] && cat /proc/mounts | greq -q -F "/home/${username}/.identity"; }  && return 0
+		while (( ${tries} < 500 )); do
+		
+			# wait until homed considers the home area active and/or the /dev/mapper/home-$username device appears and is mounted to /home/$username or the zfs keyfile appears
+			{ [[ "$(homectl inspect "${username}" | grep 'State' | sed -E s/'.*State\: '//)" == 'active' ]] || { dmsetup ls | grep -q -F "home-${username}" && cat /proc/mount | grep -F "/dev/mapper/home-${username}" | grep -q -F "/home/${username}"; } || [[ -f "$(zfs get keylocation "${zfsHomedRoot}/${username}/data" -H -o value | sed -E s/'^file\:\/\/'//)" ]]; } && break
+
+			((tries++)
 			sleep 0.2s
 		done
 	fi
 
+	# if we couldnt get a username, or have a username no /home/$username exists, or systemd-homed couldnt activate it, then return with error exit status
 	{ [[ -z "${username}" ]] || ! [[ -d "/home/${username}" ]]; } && return 1
-	#[[ "$(homectl inspect "${username}" | grep 'State' | sed -E s/'.*State\: '//)" == 'active' ]] || return 1
-	dmsetup ls | grep -q "home-${username}" || return 1
-
+	{ [[ "$(homectl inspect "${username}" | grep 'State' | sed -E s/'.*State\: '//)" == 'active' ]] || dmsetup ls | grep -q -F "home-${username}"; } || return 1
+	
+	# if ZFS homedir is already mounted and the identity file already bind-mounted then this is a unneeded duplicate call, so return
+	{ [[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] && cat /proc/mounts | greq -q -F "/home/${username}/.identity"; }  && return 0
+	
+	# load zfs key if not already loaded
 	[[ "$(zfs get keystatus "${zfsHomedRoot}/${username}/data" -H -o value)" == 'available' ]] || zfs load-key "${zfsHomedRoot}/${username}/data"
 	
+	# make tmpdir
 	mkdir -p "/tmp/zfs-homed-mount/home-${username}"
+	tmpdir="$(mktmp -p "/tmp/zfs-homed-mount/home-${username}" -d)" || tmpdir="/tmp/zfs-homed-mount/home-${username}/tmp.x}" 
+	mkdir -p "${tmpdir}"; 
 	
-	# touch "/tmp/zfs-homed-mount/home-${username}/.identity"
-	# mount -o bind,rw "/home/${username}" "/tmp/zfs-homed-mount/home-${username}"
-	# umount "/dev/mapper/home-${username}"
+	# bind mount systemd-mounted home dir to $tmpdir then umount from /home/$username
+	
+	mount -o bind,rw "/dev/mapper/home-${username}" "${tmpdir}"
+	sleep 0.2s
 	
 	umount "/dev/mapper/home-${username}" 
+	sleep 0.2s
+	
 	cat /proc/mounts | grep -q -F "/dev/mapper/home-${username}" && umount -l "/dev/mapper/home-${username}"
+	sleep 0.2s
 
-	sleep 0.5s
-
-	mount "/dev/mapper/home-${username}" "/tmp/zfs-homed-mount/home-${username}"
-
-	sleep 0.5s
-
-	#[[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] || zfs mount "${zfsHomedRoot}/${username}/data"
-	zfs mount "${zfsHomedRoot}/${username}/data" 2>/dev/null
-
-	sleep 0.5s
+	# mount zfs dataset to /home/$username
+	[[ "$(zfs get mounted "${zfsHomedRoot}/${username}/data" -H -o value)" == 'yes' ]] || zfs mount "${zfsHomedRoot}/${username}/data" 2>/dev/null
+	sleep 0.2s
 	
+	# bind mount identity file back onto /home/$username/.identity if it isnt already there
 	[[ -f "/home/${username}/.identity" ]] || touch "/home/${username}/.identity"
+	cat /proc/mounts | greq -q -F "/home/${username}/.identity" || mount -o bind,rw "/tmp/zfs-homed-mount/home-${username}/${username}/.identity" "/home/${username}/.identity"
+	sleep 0.2s
 	
-	mount -o bind,rw "/tmp/zfs-homed-mount/home-${username}/${username}/.identity" "/home/${username}/.identity"
+	# umount the systemd-homed home dir mounted at $tmpdir
 	umount "/tmp/zfs-homed-mount/home-${username}"
 	cat /proc/mounts | grep -q -F  "/tmp/zfs-homed-mount/home-${username}" && umount -l "/tmp/zfs-homed-mount/home-${username}"
 
+	# ensure $username own the identity file
 	chown "${username}":"${username}" "/home/${username}/.identity"
 
 	# echo "mounted ZFS home directory to /home/${username}" >&2
 
 	return 0
 }
+
+tmpfile_wrapper() {
+
+	local tmpfile
+	local pid_cur
+	local -a tmpfile_waitlist
+	local kk
+	
+	# make a tmpfile at /tmp/zfs-homed-mount/.tmpfiles/$USER-<...> and write pid to it
+	
+	pid_cur="${echo $$}"
+	
+	tmpfile="$(mktmp --tmp-dir="/tmp/zfs-homed-mount/.tmpfiles" --base="${*// /}-")" || tmpfile="/tmp/zfs-homed-mount/.tmpfiles/${*// /}-x"
+	
+	echo "${pid_cur}" > "${tmpfile}"
+	
+	# JOB CONTROL - make sure only one instance of zfs_homed_mount runs at a time for a given user
+	# gather all tmpfiles with the name $USER-<...> and sort oldest to youngest
+	# If the oldest one holds this process' PID, continue. f not, find where this process is in the waitlist and wait for the process just before it in the waitlist to finish
+	
+	mapfile -t tmpfile_waitlist < <(find "/tmp/zfs-homed-mount/.tmpfiles" -type f -name "${*// /}"'*' -printf '%Cs %p\n' | sort | awk '{printf $1}')
+
+	if [[ "$(cat "${tmpfile_waitlist[0]}")" != "${pid_cur}" ]]; then
+		for kk in "${!tmpfile_waitlist[@]}"; do
+			[[ "$(cat "${tmpfile_waitlist[${kk}]}")" == "${pid_cur}" ]] && wait -p "$(cat "${tmpfile_waitlist[$((( ${kk} - 1 )))]}")" && break
+		done
+	fi
+
+	zfs_homed_mount "${@}" || echo "Home area for user ${@} either could not be accessed or could not be activated" >&2
+	
+	\rm -f "${tmpfile}"	
+	
+	return 0
+}
+
+mkdir -p /tmp/zfs-homed-mount/.tmpfiles
 
 dbus-monitor --monitor --system "path='/org/freedesktop/home1',member='ActivateHome'" | while read -r dmsg; do 
 	
@@ -80,9 +137,11 @@ dbus-monitor --monitor --system "path='/org/freedesktop/home1',member='ActivateH
 
 	if ${activateFlag}; then
 		if echo "${dmsg}" | grep -q -E '^string \".+\"$'; then
-			zfs_homed_mount "$(echo "${dmsg}" | sed -E s/'^string \"(.+)\"$'/'\1'/)" 2>/dev/null &
+			homed_zfs_username="$(echo "${dmsg}" | sed -E s/'^string \"(.+)\"$'/'\1'/)"
+			tmpfile_wrapper "${homed_zfs_username}" 2>/dev/null &
+			unset homed_zfs_username
 		else
-			zfs_homed_mount 2>/dev/null &
+			tmpfile_wrapper 2>/dev/null &
 		fi
 	fi
 	activateFlag=false
